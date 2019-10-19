65df8149cd130e0c51624dd06fef3e88724a6827
Claire <clairemc3@gmail.com>
2019-10-19 19:03:41 +0100

    Refactor front end css

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="css/app.css">
+    <link rel="stylesheet" href="css/frontend.css">
     <title>Ecowash @yield('title')</title>
     <script src="https://kit.fontawesome.com/82ef6072c7.js" crossorigin="anonymous"></script>
 </head>
