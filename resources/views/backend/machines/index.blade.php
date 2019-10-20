3ff26df599d41d9db19e9f9a7cb09983f6cea12c
Claire <clairemc3@gmail.com>
2019-10-20 16:59:25 +0100

    Display machines in a table

--- resources/views/backend/machines/index.blade.php
+++ resources/views/backend/machines/index.blade.php
@@ -6,14 +6,31 @@
 
 <h1>Machines</h1>
 
-<ul>
-    @forelse ($machines as $machine)
-        <li>
-            <a href="{{ $machine->path() }}">{{ $machine->name }}</a>
-        </li>
-    @empty
-        <li>No machines here. Please add one.</li>    
-    @endforelse
-</ul>
+
+@if ($machines->isEmpty())
+    <p> You have no machines.</p>
+    <p>Please add a machine with price detailss</p>
+@else
+    <table class="table">
+        <thead>
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">Name</th>
+            <th scope="col">Price</th>
+            <th scope="col">Edit</th>
+        </tr>
+        </thead>
+        <tbody>
+        @foreach ($machines as $machine)
+        <tr>
+        <th scope="row">{{$loop->iteration}}</th>
+            <td>{{$machine->name}}</td>
+            <td>{{$machine->price}}</td>
+            <td>Edit Delete</td>
+        </tr>
+        @endforeach
+        </tbody>
+    </table>
+@endif
     
 @endSection
