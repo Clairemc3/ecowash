73218138c1fe8072fe9991b4927067423c0c0a2c
Claire <clairemc3@gmail.com>
2019-12-31 12:52:07 +0000

    Restyle the slider text

--- resources/views/frontend/sections/slider.blade.php
+++ resources/views/frontend/sections/slider.blade.php
@@ -1,6 +1,9 @@
 <div class="section">
     <div class="slider">
         <img class="slider__image" src="/images/launderette-slider-1.jpg" alt="">
+        <div class="relative">
+        <div class="slider__text">Over 10 dryers and machines available daily from 7am to 8.30pm hfj fhrje fbre jfrje hfgjre gfer gfyer fgrehj fgrejhfuyer fu reug hreughrugughrugh rehgrjeghhrejhg</div>
+     </div>
     </div>
-    <div class="slider__text">Over 10 dryers and machines available daily from 7am to 8.30pm</div>
+
 </div>
