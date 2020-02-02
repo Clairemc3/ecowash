0f9a4db617ad09744e5408392bcacfd81221e6ed
Claire <clairemc3@gmail.com>
2020-02-02 13:30:38 +0000

    Add view tests for active and inactive alerts

---
+++ tests/Feature/DisplayAlertTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class DisplayAlertTest extends TestCase
+{
+
+    use WithFaker, RefreshDatabase;
+
+    /**
+     * @test
+     *
+     * @return void
+     */
+    public function show_active_alert()
+    {
+        $activeAlert = factory('App\Alert')
+            ->create(['start_date' => now()]);
+        $response = $this->get('/');
+
+        $response->assertStatus(200)->assertSeeText($activeAlert->short_text);
+    }
+
+
+    /**
+     * @test
+     *
+     * @return void
+     */
+    public function inactive_alerts_should_not_show()
+    {
+        $inactiveAlert = factory('App\Alert')
+            ->create(['start_date' => now()->addDays(10)]);
+        $response = $this->get('/');
+
+        $response->assertStatus(200)->assertDontSeeText($inactiveAlert->short_text);
+    }
+}
