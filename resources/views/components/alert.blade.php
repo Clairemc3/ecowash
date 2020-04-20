027f917d6e6a404e618ab0617b4621d22ce293d1
Claire McEvoy <claire@electricputty.co.uk>
2020-04-20 18:44:45 +0100

    Add a component alert

---
+++ resources/views/components/alert.blade.php
@@ -0,0 +1,6 @@
+
+
+<div {{ $attributes->merge(['class' => 'flex items-center text-white text-sm font-bold mt-4 mb-4 px-4 py-3']) }}
+class="flex items-center text-white text-sm font-bold mt-4 mb-4 px-4 py-3 " role="alert">
+    <p> {{ $message }}</p>
+</div>
