fd462182ba18c550429122643edf9fe9ecdff981
Claire McEvoy <claire@electricputty.co.uk>
2020-04-25 16:49:55 +0100

    Tidy up the image select Vue component

--- resources/js/components/ImageSelect.vue
+++ resources/js/components/ImageSelect.vue
@@ -1,19 +1,16 @@
 <template>
         <modal>
             <template v-slot:trigger>
-                 <img class="w-full sm:w-1/2 md:w-1/3 m-3" v-if="selectedImage" :src="selectedImage" >
-                 <img class="w-full sm:w-1/2 md:w-1/3 m-3" v-else :src="defaultImage" >
-                 <input v-if="selectedImage" name="slider_url" type="hidden" :value="selectedImage">
+                 <img class="w-1/2 sm:w-1/3 md:w-1/4 m-3" v-if="selectedImage" :src="'/storage/' + selectedImage" >
+                 <img class="w-1/2 sm:w-1/3 md:w-1/4 m-3" v-else :src="defaultImage" >
+                 <input v-if="selectedImage" name="image_source" type="hidden" :value="'/storage/' + selectedImage">
             </template>
-            <h1 class="font-bold">Modal header</h1>
+            <h1 class="font-bold">Select an image for the slider</h1>
                 <div class="flex flex-wrap border-none overflow-y-auto max-h-350">
                     <div class="w-full sm:w-1/2 md:w-1/3 m-3" v-for="image in imageUrls" :key="image" :class="{ active: image === activeImage}">
                         <img @click="activateImage(image)" :src="imageUrl(image)">
                     </div>
                 </div>
-            <p>Modal text</p>
-            <p>jfnrjghdu</p>
-
             <template v-slot:footer>
                 <div class="button-group">
                     <button @click.prevent=selectImage class="btn btn-teal">Select</button>
