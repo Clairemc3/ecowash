4a9978f193e44eec3995b742d4897be9aaaf1852
Claire McEvoy <claire@electricputty.co.uk>
2020-05-22 14:26:03 +0100

    Add a toggle to control the status of a promotion

---
+++ resources/js/components/SwitchInput.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="field">
+        <input type="checkbox" :name="name" class="switch is-thin is-success" :checked="switchValue" value="1">
+        <label @click="switchValue = !switchValue"> {{ switchLabel }}</label>
+        <input v-if="!switchValue" type="hidden" value="0" :name="name"/>
+    </div>
+
+</template>
+
+
+<script>
+    export default {
+
+        computed: {
+            /**
+             * Returns the label to be displayed next to the switch
+             * @returns {string}
+             */
+            switchLabel() {
+                return this.switchValue
+                    ? this.activeLabel : this.inactiveLabel;
+            }
+        },
+
+        props: {
+            value: {
+                type: Boolean,
+                default: false
+            },
+            name: {
+                type: String,
+                required: true
+            },
+            activeLabel: {
+                type: String,
+                default: 'Active'
+            },
+            inactiveLabel: {
+                type: String,
+                default: 'Inactive'
+            },
+        },
+        data () {
+            return {
+                switchValue: this.value,
+            }
+        },
+    }
+</script>
+
+<style scoped>
+    .switch[type=checkbox] + label {
+        font-size: 0.75rem;
+    }
+</style>
